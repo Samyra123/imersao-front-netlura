45ef466e4898377ba514a1ec3c55a4bfad21f57d
Samyra123 <samyrds2003@gmail.com>
2026-03-31 16:46:08 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,33 @@
+export const categories = [
+    {
+        title: "Continuar Assistindo",
+        items: [
+            {
+                img: "https://i.ytimg.com/vi/BF6E_qTsJN8/sddefault.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 50,
+                youtube: "https://www.youtube.com/watch?v=L7YeC_quE-o"
+            },
+            {
+                img: "https://cdn.antaranews.com/cache/1200x800/2026/02/27/1000683051.jpg", progress: 10,
+                youtube: "https://www.youtube.com/watch?v=YC8SLpnxsL4"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://escotilha.com.br/wp-content/uploads/2015/11/serie-narcos-netflix-primeira-temporada-resenha-critica.jpeg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=04j0i_HMixE" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV8uo_XjndDisUesPXvA6efUJq4M_JyoqctQ&s", top10: true, youtube: "https://www.youtube.com/watch?v=pyi8QAlHR8k" },
+            { img: "https://i.ytimg.com/vi/DnpGe1yLNQw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAosldf1POPTp5gzYPofCjyFSbmhg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=4oDn5TBJA84" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlMsj8XYYnEyBWvDJ8l8sw0RtGrEavk3Vu8w&s", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=n7NIyrpreig" },
+        ]
+    },
+    {
+        title: "Filmes",
+        items: [
+            { img: "https://i.ytimg.com/vi/XSHjP9L0tWk/maxresdefault.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=P_-r11fcDTc" },
+            { img: "https://i.ytimg.com/vi/u_Zr0DADyIo/hqdefault.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=u_Zr0DADyIo" },
+            { img: "https://i.ytimg.com/vi/jan5CFWs9ic/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBIH8n54neLllV4Af6vIScgtZyZFQ", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=jan5CFWs9ic" },
+            { img: "https://i.ytimg.com/vi/AHMdF9KTBnY/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCpBq-SsBQmkuklE7Y3PEVrdmdz4g", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=9UfrzDKrhEc" },
+        ]
+    }
+];
